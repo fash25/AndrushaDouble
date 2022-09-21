cba1b051530ce423e0fc2fba6c752f71139ce4fb
fash25 <fash25rus@gmail.com>
2022-09-21 15:44:16 +1000

    Added Java screen configuration exsample nixyja

--- app/src/main/java/ru/synergy/andrushadouble/MainActivity.java
+++ app/src/main/java/ru/synergy/andrushadouble/MainActivity.java
@@ -1,14 +1,26 @@
 package ru.synergy.andrushadouble;
 
+import static android.widget.TextView.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+
+       // TextView.setText("Hello Android");
+
+        textView.setTextSize(22);
+        setContentView(textView);
+
+
     }
 }
