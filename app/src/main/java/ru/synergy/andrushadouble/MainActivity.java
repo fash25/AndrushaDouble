ddcb5224dfc05b935ee61e22d03a02e43d75b914
fash25 <fash25rus@gmail.com>
2022-09-27 13:40:07 +1000

    We have learned All vieva and started layouts 29

--- app/src/main/java/ru/synergy/andrushadouble/MainActivity.java
+++ app/src/main/java/ru/synergy/andrushadouble/MainActivity.java
@@ -1,13 +1,11 @@
 package ru.synergy.andrushadouble;
 
-import static android.widget.TextView.*;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,20 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-       ;
 
 
-//        setContentView(R.layout.second_layout);
-
-//        TextView textView = (TextView) findViewById(R.id.header_title);
-//        textView.setText("Hello from Java");
-
-
-//        ConstraintLayout constraintLayout = new ConstraintLayout(this);
-//        TextView textView = new TextView(this);
-//    //    TextView.setText("Hello Android");
-//        textView.setTextSize(26);
-//
 //        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
 //                ConstraintLayout.LayoutParams.WRAP_CONTENT,
 //                ConstraintLayout.LayoutParams.WRAP_CONTENT);
@@ -39,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 //        textView.setLayoutParams(layoutParams);
 //        constraintLayout.addView(textView);
 //        setContentView(constraintLayout);
-
 
     }
 }
