c5e243188f3bf3128c3a96be1b96dad9b7e85b82
fash25 <fash25rus@gmail.com>
2022-09-27 12:31:50 +1000

    We have learned LinearLayout 28 end

--- app/src/main/java/ru/synergy/andrushadouble/MainActivity.java
+++ app/src/main/java/ru/synergy/andrushadouble/MainActivity.java
@@ -5,15 +5,20 @@ import static android.widget.TextView.*;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
+       ;
+
+
 //        setContentView(R.layout.second_layout);
 
 //        TextView textView = (TextView) findViewById(R.id.header_title);
