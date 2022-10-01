162aad74ac0eecb775b46c6c309234398361964c
fash25 <fash25rus@gmail.com>
2022-10-01 17:35:53 +1000

    public class Calculator

---
+++ app/src/main/java/ru/synergy/andrushadouble/CalculatorBl.java
@@ -0,0 +1,14 @@
+package ru.synergy.andrushadouble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorBl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator_bl);
+    }
+}
